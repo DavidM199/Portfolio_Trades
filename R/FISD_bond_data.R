a5d5fb71c68f89963776f25800be4f9153da116e
DavidM199 <148761394+DavidM199@users.noreply.github.com>
2024-01-07 17:59:04 +0100

    I wrote the csvs for the extracted data.

--- R/FISD_bond_data.R
+++ R/FISD_bond_data.R
@@ -10,15 +10,15 @@ our_cusips <- our_inquiries %>% filter(!is.na(cusip)) %>% distinct(cusip)
 our_cusips <- our_cusips$cusip
 
 #EXTRACTING DATA THAT WE ACTUALLY NEED
-FISD_our_cusips <- FISD_bond_rating %>% filter(COMPLETE_CUSIP %in% our_cusips)
+FISD_rating_our_cusips <- FISD_bond_rating %>% filter(COMPLETE_CUSIP %in% our_cusips)
 
 #keeping only the rows where the rating for a bond changes compared to the last rating
-cusips_vector <- FISD_our_cusips %>% distinct(COMPLETE_CUSIP)
+cusips_vector <- FISD_rating_our_cusips %>% distinct(COMPLETE_CUSIP)
 cusips_vector <- cusips_vector[["COMPLETE_CUSIP"]]
-FISD_our_cusips$needed <- 0
+FISD_rating_our_cusips$needed <- 0
 
 for (cusip in cusips_vector){
-  bond_df <- FISD_our_cusips[cusip == FISD_our_cusips$COMPLETE_CUSIP,] 
+  bond_df <- FISD_rating_our_cusips[cusip == FISD_rating_our_cusips$COMPLETE_CUSIP,] 
   bond_df <- bond_df %>% arrange(RATING_DATE)
   current_rating <- 0
   for (i in 1:nrow(bond_df)){
@@ -27,9 +27,21 @@ for (cusip in cusips_vector){
       current_rating <- bond_df$RATING[i]
     }
   }
-  FISD_our_cusips[cusip == FISD_our_cusips$COMPLETE_CUSIP,] <- bond_df
+  FISD_rating_our_cusips[cusip == FISD_rating_our_cusips$COMPLETE_CUSIP,] <- bond_df
 }
 
-FISD_changes_only <- FISD_our_cusips %>% filter(needed == 1)
+FISD_rating_changes_only <- FISD_rating_our_cusips %>% filter(needed == 1)
+
+
+#NOW THE SAME WITH BOND ISSUE
+FISD_issue_our_cusips <- FISD_bond_issues %>% filter(COMPLETE_CUSIP %in% our_cusips)
+
+
+#write csv for the extracted rating data
+write.csv(FISD_rating_changes_only, "~/Desktop/Portfolio_Trades_my_computer/data_minimizing/FISD/FISD_rating_changes_only.csv")
+
+#write csv for the extracted issue data
+write.csv(FISD_issue_our_cusips, "~/Desktop/Portfolio_Trades_my_computer/data_minimizing/FISD/FISD_issue_our_cusips.csv")
+
 
 
