3d55b244cfd63d2330a2468204167ef78d95536b
DavidM199 <148761394+DavidM199@users.noreply.github.com>
2024-01-11 09:36:29 +0100

    commented out a part, as I will have to do that selection later

--- R/FISD_bond_data.R
+++ R/FISD_bond_data.R
@@ -6,7 +6,7 @@ FISD_bond_rating <- read_dta("~/Desktop/Portfolio_Trades_my_computer/data_minimi
 our_inquiries <- read_csv("~/Desktop/Portfolio_Trades_my_computer/data_minimizing/working_files/inquiries.csv")
 
 #gathering our cusips
-our_cusips <- our_inquiries %>% filter(!is.na(cusip)) %>% distinct(cusip)
+our_cusips <- our_inquiries %>%  filter(!is.na(cusip)) %>% distinct(cusip)
 our_cusips <- our_cusips$cusip
 
 #EXTRACTING DATA THAT WE ACTUALLY NEED
@@ -15,33 +15,33 @@ FISD_rating_our_cusips <- FISD_bond_rating %>% filter(COMPLETE_CUSIP %in% our_cu
 #keeping only the rows where the rating for a bond changes compared to the last rating
 cusips_vector <- FISD_rating_our_cusips %>% distinct(COMPLETE_CUSIP)
 cusips_vector <- cusips_vector[["COMPLETE_CUSIP"]]
-FISD_rating_our_cusips$needed <- 0
+#FISD_rating_our_cusips$needed <- 0
 
-for (cusip in cusips_vector){
-  bond_df <- FISD_rating_our_cusips[cusip == FISD_rating_our_cusips$COMPLETE_CUSIP,] 
-  bond_df <- bond_df %>% arrange(RATING_DATE)
-  current_rating <- 0
-  current_date <- 0
-  for (i in 1:nrow(bond_df)){
-    if (bond_df$RATING[i] != current_rating){
-      if (i == 1 | (bond_df$RATING_DATE[i] == current_date && bond_df$RATING_TYPE[i] != bond_df$RATING_TYPE[i-1])){
-        bond_df$needed[i] <- 1
-      }
-      else if (bond_df$RATING_DATE[i] != current_date){
-        bond_df$needed[i] <- 1
-      }
-      current_rating <- bond_df$RATING[i]
-    }
-    else if (bond_df$RATING_DATE[i] == current_date && bond_df$RATING_TYPE[i] != bond_df$RATING_TYPE[i-1]){
-      bond_df$needed[i] <- 1
-    }
-    }
-    current_date <- bond_df$RATING_DATE[i]
+#for (cusip in cusips_vector){
+#  bond_df <- FISD_rating_our_cusips[cusip == FISD_rating_our_cusips$COMPLETE_CUSIP,] 
+#  bond_df <- bond_df %>% arrange(RATING_DATE)
+#  current_rating <- 0
+#  current_date <- 0
+#  for (i in 1:nrow(bond_df)){
+#    if (bond_df$RATING[i] != current_rating){
+#      if (i == 1 | (bond_df$RATING_DATE[i] == current_date && bond_df$RATING_TYPE[i] != bond_df$RATING_TYPE[i-1])){
+#        bond_df$needed[i] <- 1
+#      }
+#      else if (bond_df$RATING_DATE[i] != current_date){
+#        bond_df$needed[i] <- 1
+#      }
+#      current_rating <- bond_df$RATING[i]
+#    }
+#    else if (bond_df$RATING_DATE[i] == current_date && bond_df$RATING_TYPE[i] != bond_df$RATING_TYPE[i-1]){
+#      bond_df$needed[i] <- 1
+#    }
+#    }
+#    current_date <- bond_df$RATING_DATE[i]
   
-  FISD_rating_our_cusips[cusip == FISD_rating_our_cusips$COMPLETE_CUSIP,] <- bond_df
-}
+#  FISD_rating_our_cusips[cusip == FISD_rating_our_cusips$COMPLETE_CUSIP,] <- bond_df
+#}
 
-FISD_rating_changes_only <- FISD_rating_our_cusips %>% filter(needed == 1)
+#FISD_rating_changes_only <- FISD_rating_our_cusips %>% filter(needed == 1)
 
 
 #NOW THE SAME WITH BOND ISSUE
@@ -49,7 +49,7 @@ FISD_issue_our_cusips <- FISD_bond_issues %>% filter(COMPLETE_CUSIP %in% our_cus
 
 
 #write csv for the extracted rating data
-write.csv(FISD_rating_changes_only, "~/Desktop/Portfolio_Trades_my_computer/data_minimizing/FISD/FISD_rating_changes_only.csv")
+write.csv(FISD_rating_our_cusips, "~/Desktop/Portfolio_Trades_my_computer/data_minimizing/FISD/FISD_rating_changes_only.csv")
 
 #write csv for the extracted issue data
 write.csv(FISD_issue_our_cusips, "~/Desktop/Portfolio_Trades_my_computer/data_minimizing/FISD/FISD_issue_our_cusips.csv")
